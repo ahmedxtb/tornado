e150debd0f95760635be93a5923af5a2882b7c87
Alyssa Frazee <acfrazee@gmail.com>
2012-07-23 18:33:41 -0400

    minor fixes

--- getTstats.R
+++ getTstats.R
@@ -11,23 +11,23 @@ getTstats = function(fit, trend = FALSE){
 	eg <- zg - digamma(fit$df.residual/2)+log(fit$df.residual/2)
 	rm(zg);gc();gc()
 	if(!trend){
-      ebar <- mean(eg)
-      G <- length(fit$sigma)
-      d0 <- 2*trigammaInverse(mean((eg-ebar)^2*(G/(G-1)) - trigamma(fit$df.residual/2)))
-      s02 <- exp(ebar+digamma(d0/2)-log(d0/2))
+	      ebar <- mean(eg)
+	      G <- length(fit$sigma)
+      	      d0 <- 2*trigammaInverse(mean((eg-ebar)^2*(G/(G-1)) - trigamma(fit$df.residual/2)))
+	      s02 <- exp(ebar+digamma(d0/2)-log(d0/2))
   	}
-	if(trend){
-      require(splines)
-      design <- try(ns(fit$Amean, df = 4, intercept = TRUE), silent = TRUE)
-      if (is(design, "try-error")) stop("Problem with Amean covariate; perhaps too few distinct values")
-      fit1 <- lm.fit(design, eg)
-      ebar <- fit1$fitted
-      evar <- mean(fit1$residuals[-(1:fit1$rank)]^2)
-      rm(fit1);gc();gc()
-      evar <- evar - mean(trigamma(fit$df.residual/2))
-      d0 <- 2*trigammaInverse(evar)
-      s02 <- exp(ebar+digamma(d0/2)-log(d0/2))
-      rm(ebar,fit1,design);gc();gc()
+	if(trend){
+	      require(splines)
+      	      design <- try(ns(fit$Amean, df = 4, intercept = TRUE), silent = TRUE)
+      	      if (is(design, "try-error")) stop("Problem with Amean covariate; perhaps too few distinct values")
+      	      fit1 <- lm.fit(design, eg)
+      	      ebar <- fit1$fitted
+      	      evar <- mean(fit1$residuals[-(1:fit1$rank)]^2)
+      	      rm(fit1);gc();gc()
+      	      evar <- evar - mean(trigamma(fit$df.residual/2))
+      	      d0 <- 2*trigammaInverse(evar)
+      	      s02 <- exp(ebar+digamma(d0/2)-log(d0/2))
+      	      rm(ebar,design);gc();gc()
   	}
 
 	sgtilde <- sqrt((d0*s02+fit$df.residual*sg2)/(d0+fit$df.residual))

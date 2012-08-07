73abe9234494936854ce7b952c9d6fad39d534c3
Alyssa Frazee <acfrazee@gmail.com>
2012-08-07 16:50:35 -0400

    added compatibility with failsafe helper function

--- getParams.R
+++ getParams.R
@@ -1,8 +1,3 @@
-## HMM (or CBS) distribution estimation function
-## functionized 6/6/12
-## added better alternative parameter estimation 7/18/12
-## alyssa frazee
-
 source("~/Documents/hopkins/research/_readlets/tornado/locfdrFit.R") 
 source("~/Documents/hopkins/research/_readlets/tornado/paramHelpers.R")
 getParams <- function(tstats, plots=FALSE, plotfile=NULL, verbose=F)
@@ -10,7 +5,7 @@ getParams <- function(tstats, plots=FALSE, plotfile=NULL, verbose=F)
 	if(plots & !is.null(plotfile)) pdf(file=plotfile)
 	pi.z = sum(tstats==0)/length(tstats)
 	nzt = tstats[tstats!=0]
-	
+
 	if(plots){
 		hist(nzt,freq=FALSE,breaks=100,xlab="t",main="Histogram of nonzero t-statistics")
 		zzaxis = seq(-5,5,length=1000)
@@ -24,7 +19,7 @@ getParams <- function(tstats, plots=FALSE, plotfile=NULL, verbose=F)
 		if(fdrmodel$needsfix==1) stop("problem with mlest parameters in locfdr")
 	}
 	if(fdrmodel.defaults$needsfix==0) fdrmodel <- fdrmodel.defaults
-	
+
 	pi.0.tmp = fdrmodel$fp0[3,3]
 	if(pi.0.tmp>=1) {
 		warning("Proportion of null nonzero bases estimated to be greater than 1.  Defaulting to 0.999.")
@@ -32,16 +27,25 @@ getParams <- function(tstats, plots=FALSE, plotfile=NULL, verbose=F)
 	}
 	pi.0 = pi.0.tmp*(1-pi.z)
 	pi.1 = 1-pi.z-pi.0
-	
+
 	if(verbose) message("starting DEup.mean...")
 	u = find.mean.up(.99, null.mean = fdrmodel$fp0[3,1], null.sd = fdrmodel$fp0[3,2], null.prop = pi.0.tmp, vals = nzt)
 	DEup.mean = u$m
 	if(verbose) message("starting DEdown.mean...")
 	d = find.mean.down(.01, null.mean = fdrmodel$fp0[3,1], null.sd = fdrmodel$fp0[3,2], null.prop = pi.0.tmp, vals = nzt)
 	DEdown.mean = d$m
-	DEup.sd = find.sd(prev.p = u$p, found.mean = DEup.mean, null.mean = fdrmodel$fp0[3,1], null.sd = fdrmodel$fp0[3,2], null.prop = pi.0.tmp, vals = nzt, up=TRUE)
-	DEdown.sd = find.sd(prev.p = d$p, found.mean = DEdown.mean, null.mean = fdrmodel$fp0[3,1], null.sd = fdrmodel$fp0[3,2], null.prop = pi.0.tmp, vals = nzt, up=FALSE)
-
+	if(names(u)[2]=="p"){
+		DEup.sd = find.sd(prev.p = u$p, found.mean = DEup.mean, null.mean = fdrmodel$fp0[3,1], null.sd = fdrmodel$fp0[3,2], null.prop = pi.0.tmp, vals = nzt, up=TRUE)		
+	}
+	if(names(u)[2]=="s"){
+		DEup.sd = u$s
+	}
+	if(names(d)[2]=="p"){
+		DEdown.sd = find.sd(prev.p = d$p, found.mean = DEdown.mean, null.mean = fdrmodel$fp0[3,1], null.sd = fdrmodel$fp0[3,2], null.prop = pi.0.tmp, vals = nzt, up=FALSE)	
+	}
+	if(names(d)[2]=="s"){
+		DEdown.sd = d$s
+	}
 	if(plots){
 		zzaxis <- seq(-10,10,length=2000)
 		hist(tstats,breaks=100,col="gray",freq=F,ylim=c(0,0.4),main="fitted distributions",xlim=c(-5,5),xlab="t statistics")
@@ -51,21 +55,10 @@ getParams <- function(tstats, plots=FALSE, plotfile=NULL, verbose=F)
 		legend("topright",lty=c(1,1,1),lwd=c(3,3,3),col=c("black","red","green"),c("null","DE up","DE down"))
 		if(!is.null(plotfile)) dev.off()
 	}
-	
+
 	params = list(mean=c(0,fdrmodel$fp0[3,1],DEup.mean,DEdown.mean),sd=c(0.0000001,fdrmodel$fp0[3,2],DEup.sd,DEdown.sd))
 	out = list(params=params, stateprobs=c(pi.z,pi.0,pi.1/2,pi.1/2),pi.z=pi.z, pi.0=pi.0, pi.1=pi.1)
 	return(out)
 }
-
-#test:
-load("~/Documents/hopkins/research/_readlets/_zeiger-readlets/st22.rda")
-#getParams(st,plots=F,plotfile="~/Documents/hopkins/research/_ebHMM-Rfunctions/testplot.pdf")
-getParams(st,plots=T)
-
-# test on different data:
-#load("~/Documents/hopkins/research/_sarven-brainSamples/st/st22.rda")
-#getParams(st,plots=T,plotfile="~/Documents/hopkins/research/_ebHMM-Rfunctions/testplot-sarven.pdf")
-
-
 
 

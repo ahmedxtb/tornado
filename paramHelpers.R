1dd25333c4393d3a3b2c5d51f1acb4dbc0e6d30c
Alyssa Frazee <acfrazee@gmail.com>
2012-08-07 16:47:15 -0400

    added failsafe function

--- paramHelpers.R
+++ paramHelpers.R
@@ -1,7 +1,38 @@
 # helper functions for getParams.R
 # 7/18/12
+# updated 8/7/12 to include the failsafe function
 
 last <- function(x) return(tail(x, n=1))
+
+getParams.failsafe <- function(){
+	# this shouldn't need any arguments, since it's used entirely within the other functions.
+	# used if the other method fails.
+			if(!exists("fdrmodel")) stop("fdrmodel must be defined")
+			bincounts = fdrmodel$yt
+			binlocs = fdrmodel$x
+			downinds = which(binlocs<0)
+			upinds = which(binlocs>=0)
+			oecx = binlocs[upinds]
+			oecy = bincounts[upinds]
+			uecx = binlocs[downinds]
+			uecy = bincounts[downinds]
+			mystats <- mystats2 <- list()
+			for(i in 1:length(oecx)){
+                          if(oecy[i]==0) mystats[[i]] <- NA
+                          if(oecy[i]!=0) mystats[[i]] <- rep(oecx[i],round(oecy[i]))
+			}
+			mystats <- unlist(mystats)
+			DEup.sd <- sd(mystats,na.rm=TRUE)
+			DEup.mean <- mean(mystats,na.rm=TRUE)
+			for(i in 1:length(uecx)){
+                          if(uecy[i]==0) mystats2[[i]] <- NA
+                          if(uecy[i]!=0) mystats2[[i]] <- rep(uecx[i],round(uecy[i]))
+			}
+			mystats2 <- unlist(mystats2)
+			DEdown.sd <- sd(mystats2,na.rm=TRUE)
+			DEdown.mean <- mean(mystats2,na.rm=TRUE)
+			return(list(DEup.mean = DEup.mean, DEdown.mean = DEdown.mean, DEup.sd = DEup.sd, DEdown.sd = DEdown.sd))
+}
 
 get.numalts <- function(pctil,null.mean,null.sd,null.prop,vals,up = TRUE){
 	cutoff.val = qnorm(pctil,null.mean,null.sd)
@@ -15,22 +46,40 @@ get.numalts <- function(pctil,null.mean,null.sd,null.prop,vals,up = TRUE){
 	return(list(num=round(num.alts.above),val=cutoff.val))
 }
 
-find.mean.up <- function(init.value, null.mean, null.sd, null.prop, vals, verbose=F){
+find.mean.up <- function(init.value, null.mean, null.sd, null.prop, vals){
 	if(init.value<=0.5) stop("finding DE up quantile - init.value should be >0.5")
 	x = init.value
 	target = round(0.5*0.5*(1-null.prop)*length(vals))  #half the expected # of DEup values.
-	history = pseq = NULL
+	history = pseq = numseq = qseq = NULL
 	counter = 0
 	while(TRUE){
-		iter = get.numalts(x,null.mean,null.sd,null.prop,vals,up=T)
+		iter = try(get.numalts(x,null.mean,null.sd,null.prop,vals,up=T),silent=T)
+		if(class(iter)=="try-error") {
+			warning("Numerical estimation of DE-up mean failed.  Using locfdr estimates.")
+			gp = getParams.failsafe()
+			return(list(m=gp$DEup.mean, s=gp$DEup.sd))
+		}
 		if(iter$num == target) return(list(m=iter$val,p = pseq[counter]))
 		if(counter>1){
 			if(abs(pseq[counter]-pseq[counter-1])<1e-8) return(list(m=iter$val,p=pseq[counter]))
 		}
 		counter = counter+1
-		if(verbose) print(counter)
+		numseq[counter] = iter$num
+		qseq[counter] = iter$val
 		pseq[counter] = x
-		if(verbose) print(x)
+		
+		if(counter>1){
+			if(history[counter-1]==1 & numseq[counter]>=numseq[counter-1]){
+				warning("numerical estimation of DE-up mean failed.  Using locfdr estimates.")
+				gp = getParams.failsafe()
+				return(list(m=gp$DEup.mean, s=gp$DEup.sd))
+			}
+			if(history[counter-1]==-1 & numseq[counter]<=numseq[counter-1]){
+				warning("numerical estimation of DE-up mean failed.  Using locfdr estimates.")
+				gp = getParams.failsafe()
+				return(list(m=gp$DEup.mean, s=gp$DEup.sd))
+			}
+		}
 		
 		if(iter$num > target){
 			history[counter] = 1
@@ -51,22 +100,40 @@ find.mean.up <- function(init.value, null.mean, null.sd, null.prop, vals, verbos
 		}
 }
 
-find.mean.down <- function(init.value, null.mean, null.sd, null.prop, vals, verbose=F){
+find.mean.down <- function(init.value, null.mean, null.sd, null.prop, vals){
 	if(init.value>=0.5) stop("finding DE down quantile - init.value should be <0.5")
 	x = init.value
 	target = round(0.5*0.5*(1-null.prop)*length(vals))  #half the expected # of DEdown values.
-	history = pseq = NULL
+	history = pseq = numseq = qseq = NULL
 	counter = 0
 	while(TRUE){
-		iter = get.numalts(x,null.mean,null.sd,null.prop,vals,up=F)
+		iter = try(get.numalts(x,null.mean,null.sd,null.prop,vals,up=F),silent=T)
+		if(class(iter)=="try-error") {
+			warning("numerical estimation of DE-down mean failed. Using locfdr estimates.")
+			gp = getParams.failsafe()
+			return(list(m=gp$DEdown.mean,s=gp$DEdown.sd))
+		}
 		if(iter$num == target) return(list(m=iter$val,p = pseq[counter]))
 		if(counter>1){
 			if(abs(pseq[counter]-pseq[counter-1])<1e-8) return(list(m=iter$val,p=pseq[counter]))
 		}
 		counter = counter+1
-		if(verbose) print(counter)
+		numseq[counter] = iter$num
+		qseq[counter] = iter$val
 		pseq[counter] = x
-		if(verbose) print(x)
+		
+		if(counter>1){
+			if(history[counter-1]==1 & numseq[counter]>=numseq[counter-1]){
+				warning("numerical estimation of DE-down mean failed.  Using locfdr estimates.")
+				gp = getParams.failsafe()
+				return(list(m=gp$DEdown.mean, s=gp$DEdown.sd))
+			}
+			if(history[counter-1]==-1 & numseq[counter]<=numseq[counter-1]){
+				warning("numerical estimation of DE-down mean failed.  Using locfdr estimates.")
+				gp = getParams.failsafe()
+				return(list(m=gp$DEdown.mean, s=gp$DEdown.sd))
+			}
+		}
 		
 		if(iter$num > target){
 			history[counter] = 1
@@ -103,14 +170,28 @@ find.sd <- function(prev.p, found.mean, null.mean, null.sd, null.prop, vals, up=
 	num.nulls.above = coef1*num.nulls
 	num.total.above = ifelse(up,sum(vals>cutoff.val),sum(vals<cutoff.val))
 	num.alts.above = num.total.above - num.nulls.above
-	if(num.alts.above<=0) stop("yucky! num.alts.above<0!")
 	num.alts = (1-null.prop)*0.5*length(vals) # this is the number of DEup values
 	if(up) alt.percentile = 1-(num.alts.above/num.alts)
 	if(!up) alt.percentile = num.alts.above/num.alts
 	zstat = qnorm(alt.percentile)
+	if(is.na(zstat)){
+		warning("Numerical standard deviation estimation failed.  Using locfdr estimates.")
+		gp = getParams.failsafe()
+		if(up) return(gp$DEup.sd)
+		if(!up) return(gp$DEdown.sd)
+	}
 	sigma = (cutoff.val - found.mean)/zstat
+    if(num.alts.above<=0 | sigma<=0) {
+		warning("Numerical standard deviation estimation failed. Using locfdr estimates.")
+		gp = getParams.failsafe()
+		if(up) return(gp$DEup.sd)
+		if(!up) return(gp$DEdown.sd)
+	}
 	return(sigma)
 }
+
+
+
 
 
 
